6aac61fe6d874c1516d75d852ee4e63ac5ae088d
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-29 17:09:58 +0800

    Updated NpgsqlProcessHelper.cs

--- Scarlet.Npgsql/Rdbms/NpgsqlProcessHelper.cs
+++ Scarlet.Npgsql/Rdbms/NpgsqlProcessHelper.cs
@@ -1,8 +1,6 @@
 ﻿using Npgsql;
 using System;
-using System.Collections.Generic;
 using System.Security;
-using System.Threading.Tasks;
 
 namespace Scarlet.Rdbms
 {
@@ -19,46 +17,6 @@ namespace Scarlet.Rdbms
         {
             ConnectionEstablisher.Dispose();
             ConnectionEstablisher = null;
-        }
-
-        public T ExecuteReader<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<NpgsqlDataReader, T> fromReader)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> ExecuteReaderAsync<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<NpgsqlDataReader, T> fromReader)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> ExecuteReaderAsync<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<NpgsqlDataReader, Task<T>> fromReaderAsync)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<T> ExecuteReaderIEnumerable<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<NpgsqlDataReader, T> fromReader)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<T>> ExecuteReaderIEnumerableAsync<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<NpgsqlDataReader, T> fromReader)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<T>> ExecuteReaderIEnumerableAsync<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<NpgsqlDataReader, Task<T>> fromReaderAsync)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T ExecuteScalar<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<object, T> converter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> ExecuteScalarAsync<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<object, T> converter)
-        {
-            throw new NotImplementedException();
         }
     }
 }
