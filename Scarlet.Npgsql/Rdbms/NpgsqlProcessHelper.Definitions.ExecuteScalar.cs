2147e2f25299af980e374f0a746db68c82aed945
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-29 17:09:42 +0800

    Added NpgsqlProcessHelper.Definitions.ExecuteScalar.cs

---
+++ Scarlet.Npgsql/Rdbms/NpgsqlProcessHelper.Definitions.ExecuteScalar.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace Scarlet.Rdbms
+{
+    partial class NpgsqlProcessHelper
+    {
+        public T ExecuteScalar<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<object, T> converter)
+        {
+            using (var connection = ConnectionEstablisher.Establish())
+            {
+                using (var command = createCommand(connection))
+                {
+                    return converter(command.ExecuteScalar());
+                }
+            }
+        }
+
+        public async Task<T> ExecuteScalarAsync<T>(Func<NpgsqlConnection, NpgsqlCommand> createCommand, Func<object, T> converter)
+        {
+            using (var connection = await ConnectionEstablisher.EstablishAsync())
+            {
+                using (var command = createCommand(connection))
+                {
+                    return converter(await command.ExecuteScalarAsync());
+                }
+            }
+        }
+    }
+}
