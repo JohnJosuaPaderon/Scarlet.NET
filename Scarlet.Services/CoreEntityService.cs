124c735bf05b78031df17b489a48a0a746b58de1
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-29 17:29:00 +0800

    Various updates.

--- Scarlet.Services/CoreEntityService.cs
+++ Scarlet.Services/CoreEntityService.cs
@@ -2,28 +2,12 @@
 using System;
 using System.Threading.Tasks;
 using Scarlet.Core.Entities;
-using System.Threading;
 
 namespace Scarlet.Services
 {
     public class CoreEntityService : ICoreEntityService
     {
-        public Person DeletePerson(Person person)
-        {
-            throw new NotImplementedException();
-        }
-
         public Task<Person> DeletePersonAsync(Person person)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Person> DeletePersonAsync(Person person, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Person InsertPerson(Person person)
         {
             throw new NotImplementedException();
         }
@@ -33,22 +17,7 @@ namespace Scarlet.Services
             throw new NotImplementedException();
         }
 
-        public Task<Person> InsertPersonAsync(Person person, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Person UpdatePerson(Person person)
-        {
-            throw new NotImplementedException();
-        }
-
         public Task<Person> UpdatePersonAsync(Person person)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Person> UpdatePersonAsync(Person person, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
