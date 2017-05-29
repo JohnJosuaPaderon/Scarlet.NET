f7f39ddb27a2fdbf8531366bb5a3698399d42dcf
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-29 10:44:32 +0800

    Various updates.

---
+++ Scarlet/Core/EntityManagementService/ICoreEntityService.cs
@@ -0,0 +1,29 @@
+﻿using Scarlet.Core.Entities;
+using System.ServiceModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scarlet.Core.EntityManagementService
+{
+    [ServiceContract]
+    public interface ICoreEntityService
+    {
+        [OperationContract]
+        Person InsertPerson(Person person);
+
+        [OperationContract]
+        Task<Person> InsertPersonAsync(Person person);
+
+        [OperationContract]
+        Task<Person> InsertPersonAsync(Person person, CancellationToken cancellationToken);
+
+        [OperationContract]
+        Person UpdatePerson(Person person);
+
+        [OperationContract]
+        Task<Person> UpdatePersonAsync(Person person);
+
+        [OperationContract]
+        Task<Person> UpdatePersonAsync(Person person, CancellationToken cancellationToken);
+    }
+}
