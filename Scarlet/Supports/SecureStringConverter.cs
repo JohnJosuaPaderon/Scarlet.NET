911a78488b92d8642a7ca4a42689178510b450da
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-29 14:19:11 +0800

    Added SecureStringConverter class.

---
+++ Scarlet/Supports/SecureStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security;
+
+namespace Scarlet.Supports
+{
+    public static class SecureStringConverter
+    {
+        public static string FromSecureString(SecureString value)
+        {
+            if (value != null)
+            {
+                throw new NotImplementedException();
+                // TODO:
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static SecureString ToSecureString(string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                throw new NotImplementedException();
+                // TODO:
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
