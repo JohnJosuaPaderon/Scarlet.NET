55db2c57b8c50e782a3d37d91d9116b781e29745
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-29 17:27:39 +0800

    Removed Scarlet.Core project.

---
+++ [DEPRECIATED] Scarlet.Core/EntityManagers/PersonManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Scarlet.Core.Entities;
+using Scarlet.Core.EntityProcesses;
+
+namespace Scarlet.Core.EntityManagers
+{
+    public sealed class PersonManager : IPersonManager
+    {
+        public string ConstructFullName(Person person)
+        {
+            if (person != null)
+            {
+                using (var process = new ConstructPersonFullName())
+                {
+                    process.Person = person;
+                    return process.Execute();
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string ConstructInformalFullName(Person person)
+        {
+            if (person != )
+            {
+
+            }
+
+        }
+
+        public string ConstructMiddleInitial(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person> InsertAsync(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
