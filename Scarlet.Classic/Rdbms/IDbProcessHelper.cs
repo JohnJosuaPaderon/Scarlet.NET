840e348143a22329fdf0551e936a6b3d531fc0aa
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-29 16:16:24 +0800

    Updated IDbProcessHelper interface.

--- Scarlet.Classic/Rdbms/IDbProcessHelper.cs
+++ Scarlet.Classic/Rdbms/IDbProcessHelper.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Common;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Scarlet.Rdbms
@@ -12,49 +12,37 @@ namespace Scarlet.Rdbms
         where TParameter : DbParameter
         where TDbDataReader : DbDataReader 
     {
-        int HandleExecute(Func<TConnection, int> execute);
-        int HandleExecute(Func<TConnection, TTransaction, int> execute);
-        Task<int> HandleExecuteAsync(Func<TConnection, int> execute);
-        Task<int> HandleExecuteAsync(Func<TConnection, TTransaction, int> execute);
-        Task<int> HandleExecuteAsync(Func<TConnection, int> execute, CancellationToken cancellationToken);
-        Task<int> HandleExecuteAsync(Func<TConnection, TTransaction, int> execute, CancellationToken cancellationToken);
-        Task<int> HandleExecuteAsync(Func<TConnection, Task<int>> executeAsync);
-        Task<int> HandleExecuteAsync(Func<TConnection, TTransaction, Task<int>> executeAsync);
-        Task<int> HandleExecuteAsync(Func<TConnection, CancellationToken, Task<int>> executeAsync, CancellationToken cancellationToken);
-        Task<int> HandleExecuteAsync(Func<TConnection, TTransaction, CancellationToken, Task<int>> executeAsync, CancellationToken cancellationToken);
-        T HandleExecute<T>(Func<TConnection, T> execute);
-        T HandleExecute<T>(Func<TConnection, TTransaction, T> execute);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, T> execute);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TTransaction, T> execute);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, T> execute, CancellationToken cancellationToken);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TTransaction, T> execute, CancellationToken cancellationToken);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, Task<T>> executeAsync);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TTransaction, Task<T>> executeAsync);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, CancellationToken, Task<T>> executeAsync, CancellationToken cancellationToken);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TTransaction, CancellationToken, Task<T>> executeAsync, CancellationToken cancellationToken);
-        int HandleExecute(Func<TConnection, TCommand> createCommand);
-        int HandleExecute(Func<TConnection, TTransaction, TCommand> createCommand);
-        Task<int> HandleExecuteAsync(Func<TConnection, TCommand> createCommand);
-        Task<int> HandleExecuteAsync(Func<TConnection, TTransaction, TCommand> createCommand);
-        Task<int> HandleExecuteAsync(Func<TConnection, TCommand> createCommand, CancellationToken cancellationToken);
-        Task<int> HandleExecuteAsync(Func<TConnection, TTransaction, TCommand> createCommand, CancellationToken cancellationToken);
-        T HandleExecute<T>(Func<TConnection, TCommand> createCommand);
-        T HandleExecute<T>(Func<TConnection, TTransaction, TCommand> createCommand);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TCommand> createCommand);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TCommand> createCommand, CancellationToken cancellationToken);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, CancellationToken cancellationToken);
-        void HandleExecute(Func<TConnection, TCommand> createCommand, Action<int, TCommand> postAction);
-        void HandleExecute(Func<TConnection, TTransaction, TCommand> createCommand, Action<int, TCommand> postAction);
-        Task HandleExecuteAsync(Func<TConnection, TCommand> createCommand, Action<int, TCommand> postAction);
-        Task HandleExecuteAsync(Func<TConnection, TTransaction, TCommand> createCommand, Action<int, TCommand> postAction);
-        Task HandleExecuteAsync(Func<TConnection, TCommand> createCommand, Action<int, TCommand> postAction, CancellationToken cancellationToken);
-        Task HandleExecuteAsync(Func<TConnection, TTransaction, TCommand> createCommand, Action<int, TCommand> postAction, CancellationToken cancellationToken);
-        T HandleExecute<T>(Func<TConnection, TCommand> createCommand, Func<int, TCommand, T> postAction);
-        T HandleExecute<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<int, TCommand, T> postAction);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TCommand> createCommand, Func<int, TCommand, T> postAction);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<int, TCommand, T> postAction);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TCommand> createCommand, Func<int, TCommand, T> postAction, CancellationToken cancellationToken);
-        Task<T> HandleExecuteAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<int, TCommand, T> postAction, CancellationToken cancellationToken);
+        T ExecuteNonQuery<T>(Func<TConnection, T> execute);
+        T ExecuteNonQuery<T>(Func<TConnection, TTransaction, T> execute);
+        Task<T> ExecuteNonQueryAsync<T>(Func<TConnection, T> execute);
+        Task<T> ExecuteNonQueryAsync<T>(Func<TConnection, TTransaction, T> execute);
+        Task<T> ExecuteNonQueryAsync<T>(Func<TConnection, Task<T>> executeAsync);
+        Task<T> ExecuteNonQueryAsync<T>(Func<TConnection, TTransaction, Task<T>> executeAsync);
+        T ExecuteNonQuery<T>(Func<TConnection, TCommand> createCommand);
+        T ExecuteNonQuery<T>(Func<TConnection, TTransaction, TCommand> createCommand);
+        Task<T> ExecuteNonQueryAsync<T>(Func<TConnection, TCommand> createCommand);
+        Task<T> ExecuteNonQueryAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand);
+        Task ExecuteNonQueryAsync(Func<TConnection, TCommand> createCommand, Action<int, TCommand> postAction);
+        Task ExecuteNonQueryAsync(Func<TConnection, TTransaction, TCommand> createCommand, Action<int, TCommand> postAction);
+        T ExecuteNonQuery<T>(Func<TConnection, TCommand> createCommand, Func<int, TCommand, T> postAction);
+        T ExecuteNonQuery<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<int, TCommand, T> postAction);
+        Task<T> ExecuteNonQueryAsync<T>(Func<TConnection, TCommand> createCommand, Func<int, TCommand, T> postAction);
+        Task<T> ExecuteNonQueryAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<int, TCommand, T> postAction);
+        T ExecuteReader<T>(Func<TConnection, TCommand> createCommand, Func<TDbDataReader, T> fromReader);
+        T ExecuteReader<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<TDbDataReader, T> fromReader);
+        Task<T> ExecuteReaderAsync<T>(Func<TConnection, TCommand> createCommand, Func<TDbDataReader, T> fromReader);
+        Task<T> ExecuteReaderAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<TDbDataReader, T> fromReader);
+        Task<T> ExecuteReaderAsync<T>(Func<TConnection, TCommand> createCommand, Func<TDbDataReader, Task<T>> fromReaderAsync);
+        Task<T> ExecuteReaderAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<TDbDataReader, Task<T>> fromReaderAsync);
+        IEnumerable<T> ExecuteReaderIEnumerable<T>(Func<TConnection, TCommand> createCommand, Func<TDbDataReader, T> fromReader);
+        IEnumerable<T> ExecuteReaderIEnumerable<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<TDbDataReader, T> fromReader);
+        Task<IEnumerable<T>> ExecuteReaderIEnumerableAsync<T>(Func<TConnection, TCommand> createCommand, Func<TDbDataReader, T> fromReader);
+        Task<IEnumerable<T>> ExecuteReaderIEnumerableAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<TDbDataReader, T> fromReader);
+        Task<IEnumerable<T>> ExecuteReaderIEnumerableAsync<T>(Func<TConnection, TCommand> createCommand, Func<TDbDataReader, Task<T>> fromReaderAsync);
+        Task<IEnumerable<T>> ExecuteReaderIEnumerableAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<TDbDataReader, Task<T>> fromReaderAsync);
+        T ExecuteScalar<T>(Func<TConnection, TCommand> createCommand, Func<object, T> converter);
+        T ExecuteScalar<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<object> converter);
+        Task<T> ExecuteScalarAsync<T>(Func<TConnection, TCommand> createCommand, Func<object, T> converter);
+        Task<T> ExecuteScalarAsync<T>(Func<TConnection, TTransaction, TCommand> createCommand, Func<object, T> converter);
     }
 }
