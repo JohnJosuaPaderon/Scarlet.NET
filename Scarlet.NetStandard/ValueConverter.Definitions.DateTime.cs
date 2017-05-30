343d6823cf65f225eb63cac1ddbf833c8ecbb13f
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 12:31:50 +0800

    Added ValueConverter.Definitions.DateTime.cs

---
+++ Scarlet.NetStandard/ValueConverter.Definitions.DateTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Scarlet
+{
+    partial class ValueConverter
+    {
+        public static DateTime ToDateTime(object value)
+        {
+            return ConversionBase(value, Convert.ToDateTime);
+        }
+
+        public static DateTime ToDateTime(object value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, Convert.ToDateTime);
+        }
+
+        public static DateTime ToDateTime(string value)
+        {
+            return ConversionBase(value, Convert.ToDateTime);
+        }
+
+        public static DateTime ToDateTime(string value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, Convert.ToDateTime);
+        }
+    }
+}
