47e58a0ceb0c8018051edd5c9cc23c8b872daa95
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 21:19:55 +0800

    Added User.cs Added UserLoginCredentials.cs

---
+++ Scarlet.NetStandard/Security/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Scarlet.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Scarlet.Security.Entities
+{
+    [DataContract]
+    public class User
+    {
+
+    }
+}
---
+++ Scarlet.NetStandard/Security/Entities/UserLoginCredentials.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Scarlet.Security.Entities
+{
+    [DataContract]
+    public class UserLoginCredentials
+    {
+    }
+}
