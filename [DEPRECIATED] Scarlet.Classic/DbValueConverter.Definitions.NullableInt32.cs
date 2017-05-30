3c5f8dbacaec1f2c08ab8eabcb846a47562e84da
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 08:43:57 +0800

    Removed Scarlet.Classic Project.

---
+++ [DEPRECIATED] Scarlet.Classic/DbValueConverter.Definitions.NullableInt32.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Scarlet
+{
+    partial class DbValueConverter
+    {
+        public static int? ToNullableInt32(bool value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(byte value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(char value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(decimal value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(double value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(short value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(int value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(long value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(object value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(object value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(sbyte value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(float value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(string value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(string value, int fromBase)
+        {
+            return ConversionBase(value, fromBase, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(string value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(ushort value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(uint value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+
+        public static int? ToNullableInt32(ulong value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableInt32);
+        }
+    }
+}
---
+++ [DEPRECIATED] Scarlet.Classic/Rdbms/IDbCommandCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Common;
+
+namespace Scarlet.Rdbms
+{
+    public interface IDbCommandCreator<TConnection, TTransaction, TCommand, TParameter>
+        where TConnection : DbConnection
+        where TTransaction : DbTransaction
+        where TCommand : DbCommand
+        where TParameter : DbParameter
+    {
+        TCommand CreateCommand(string commandText, TConnection connection);
+        TCommand CreateCommand(string commandText, TConnection connection, params TParameter[] parameters);
+        TCommand CreateCommand(string commandText, TConnection connection, TTransaction transaction);
+        TCommand CreateCommand(string commandText, TConnection connection, TTransaction transaction, params TParameter[] parameters);
+        TCommand CreateProcedureCommand(string storedProcedure, TConnection connection);
+        TCommand CreateProcedureCommand(string storedProcedure, TConnection connection, params TParameter[] parameters);
+        TCommand CreateProcedureCommand(string storedProcedure, TConnection connection, TTransaction transaction);
+        TCommand CreateProcedureCommand(string storedProcedure, TConnection connection, TTransaction transaction, params TParameter[] parameters);
+    }
+}
