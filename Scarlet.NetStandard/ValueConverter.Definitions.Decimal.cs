50332d749876809d709406aca991f4b99e0cec22
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 12:31:58 +0800

    Added ValueConverter.Definitions.Decimal.cs

---
+++ Scarlet.NetStandard/ValueConverter.Definitions.Decimal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Scarlet
+{
+    partial class ValueConverter
+    {
+        public static decimal ToDecimal(bool value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(byte value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(decimal value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(double value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(short value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(int value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(long value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(object value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(object value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(sbyte value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(float value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(string value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(string value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(ushort value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(uint value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+
+        public static decimal ToDecimal(ulong value)
+        {
+            return ConversionBase(value, Convert.ToDecimal);
+        }
+    }
+}
