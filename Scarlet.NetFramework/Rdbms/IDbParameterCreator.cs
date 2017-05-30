efb6ee1843b3f6dfb0a001a019423153213a8374
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 08:17:19 +0800

    Added IDbParameterCreator.cs

---
+++ Scarlet.NetFramework/Rdbms/IDbParameterCreator.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Common;
+
+namespace Scarlet.Rdbms
+{
+    public interface IDbParameterCreator<TParameter>
+        where TParameter : DbParameter
+    {
+        TParameter CreateInput(string parameterName, object value);
+        TParameter CreateOutput(string parameterName);
+        TParameter CreateInOut(string parameterName, object initialValue);
+    }
+}
