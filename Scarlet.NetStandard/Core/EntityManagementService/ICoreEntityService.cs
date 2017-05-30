167cdb199b3d975962b3a1ccf42421287fc2869e
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 12:27:19 +0800

    Added ICoreEntityService.cs

---
+++ Scarlet.NetStandard/Core/EntityManagementService/ICoreEntityService.cs
@@ -0,0 +1,19 @@
+﻿using Scarlet.Core.Entities;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Scarlet.Core.EntityManagementService
+{
+    [ServiceContract]
+    public interface ICoreEntityService
+    {
+        [OperationContract]
+        Task<Person> InsertPersonAsync(Person person);
+        
+        [OperationContract]
+        Task<Person> UpdatePersonAsync(Person person);
+
+        [OperationContract]
+        Task<Person> DeletePersonAsync(Person person);
+    }
+}
