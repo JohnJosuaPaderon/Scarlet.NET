f57f6fdfd5b192533f6f4053039b388e696699c7
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 12:35:24 +0800

    Added ValueConverter.Definitions.NullableInt64.cs Added ValueConverter.Definitions.NullableSByte.cs Added ValueConverter.Definitions.NullableSingle.cs Added ValueConverter.Definitions.NullableUInt16.cs Added ValueConverter.Definitions.NullableUInt32.cs Added ValueConverter.Definitions.NullableUInt64.cs Added ValueConverter.Definitions.SByte.cs Added ValueConverter.Definitions.Single.cs Added ValueConverter.Definitions.String.cs Added ValueConverter.Definitions.UInt16.cs Added ValueConverter.Definitions.UInt32.cs Added ValueConverter.Definitions.UInt.64cs

---
+++ Scarlet.NetStandard/ValueConverter.Definitions.NullableUInt32.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Scarlet
+{
+    partial class ValueConverter
+    {
+        public static uint? ToNullableUInt32(bool value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(byte value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(char value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(decimal value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(double value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(short value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(int value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(long value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(object value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(object value, IFormatProvider formatProvider)
+        {
+            return NullableConversionBase(value, formatProvider, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(sbyte value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(float value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(string value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(string value, int fromBase)
+        {
+            return NullableConversionBase(value, fromBase, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(string value, IFormatProvider formatProvider)
+        {
+            return NullableConversionBase(value, formatProvider, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(ushort value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(uint value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+
+        public static uint? ToNullableUInt32(ulong value)
+        {
+            return NullableConversionBase(value, Convert.ToUInt32);
+        }
+    }
+}
