9d2dc23e23c7846f269058e727e205cee7a050ff
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 12:27:29 +0800

    Added IPersonManager.cs

---
+++ Scarlet.NetStandard/Core/EntityManagers/IPersonManager.cs
@@ -0,0 +1,13 @@
+﻿using Scarlet.Core.Entities;
+using System.Threading.Tasks;
+
+namespace Scarlet.Core.EntityManagers
+{
+    public interface IPersonManager
+    {
+        string ConstructFullName(Person person);
+        string ConstructInformalFullName(Person person);
+        string ConstructMiddleInitial(Person person);
+        Task<Person> InsertAsync(Person person);
+    }
+}
