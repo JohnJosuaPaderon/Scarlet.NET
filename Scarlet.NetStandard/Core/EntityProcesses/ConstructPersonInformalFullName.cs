f04b6dc1b94ef1dfa0b8c94f7cda3e678983c9c9
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 12:27:55 +0800

    Added ConstructPersonInformalFullName.cs

---
+++ Scarlet.NetStandard/Core/EntityProcesses/ConstructPersonInformalFullName.cs
@@ -0,0 +1,20 @@
+﻿using Scarlet.Core.Entities;
+using System;
+
+namespace Scarlet.Core.EntityProcesses
+{
+    public sealed class ConstructPersonInformalFullName : IConstructPersonInformalFullName
+    {
+        public Person Person { get; set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
