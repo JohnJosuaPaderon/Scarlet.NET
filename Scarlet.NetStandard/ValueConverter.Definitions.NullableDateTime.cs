af46d053065ae2db756096901dd00ffbfbb4ba33
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-30 12:33:17 +0800

    Added ValueConverter.Definitions.NullableDateTime.cs

---
+++ Scarlet.NetStandard/ValueConverter.Definitions.NullableDateTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Scarlet
+{
+    partial class ValueConverter
+    {
+        public static DateTime? ToNullableDateTime(object value)
+        {
+            return NullableConversionBase(value, Convert.ToDateTime);
+        }
+
+        public static DateTime? ToNullableDateTime(object value, IFormatProvider formatProvider)
+        {
+            return NullableConversionBase(value, formatProvider, Convert.ToDateTime);
+        }
+
+        public static DateTime? ToNullableDateTime(string value)
+        {
+            return NullableConversionBase(value, Convert.ToDateTime);
+        }
+
+        public static DateTime? ToNullableDateTime(string value, IFormatProvider formatProvider)
+        {
+            return NullableConversionBase(value, formatProvider, Convert.ToDateTime);
+        }
+    }
+}
