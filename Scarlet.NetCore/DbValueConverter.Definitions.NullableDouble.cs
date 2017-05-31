b785a28a5345bf1146ccb49ffb1ad352bc511fc8
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-31 16:18:25 +0800

    Added DbValueConverter.cs Added DbValueConverter.Definitions.Boolean.cs Added DbValueConverter.Definitions.Byte.cs Added DbValueConverter.Definitions.Char.cs Added DbValueConverter.Definitions.DateTime.cs Added DbValueConverter.Definitions.Decimal.cs Added DbValueConverter.Definitions.Double.cs Added DbValueConverter.Definitions.Int16.cs Added DbValueConverter.Definitions.Int32.cs Added DbValueConverter.Definitions.Int64.cs Added DbValueConverter.Definitions.NullableBoolean.cs Added DbValueConverter.Definitions.NullableByte.cs Added DbValueConverter.Definitions.NullableChar.cs Added DbValueConverter.Definitions.NullableDateTime.cs Added DbValueConverter.Definitions.NullableDecimal.cs Added DbValueConverter.Definitions.NullableDouble.cs Added DbValueConverter.Definitions.NullableInt16.cs Added DbValueConverter.Definitions.NullableInt32.cs Added DbValueConverter.Definitions.NullableInt64.cs Added DbValueConverter.Definitions.NullableSByte.cs Added DbValueConverter.Definitions.NullableSingle.cs Added DbValueConverter.Definitions.NullableUInt16.cs Added DbValueConverter.Definitions.NullableUInt32.cs Added DbValueConverter.Definitions.NullableUInt64.cs Added DbValueConverter.Definitions.SByte.cs Added DbValueConverter.Definitions.Single.cs Added DbValueConverter.Definitions.String.cs Added DbValueConverter.Definitions.UInt16.cs Added DbValueConverter.Definitions.UInt32.cs Added DbValueConverter.Definitions.UInt64.cs Added Scarlet.NetCore.csproj

---
+++ Scarlet.NetCore/DbValueConverter.Definitions.NullableDouble.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Scarlet
+{
+    partial class DbValueConverter
+    {
+        public static double? ToNullableDouble(bool value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(byte value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(decimal value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(double value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(short value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(int value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(long value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(object value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(object value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(sbyte value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(float value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(string value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(string value, IFormatProvider formatProvider)
+        {
+            return ConversionBase(value, formatProvider, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(ushort value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(uint value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+
+        public static double? ToNullableDouble(ulong value)
+        {
+            return ConversionBase(value, ValueConverter.ToNullableDouble);
+        }
+    }
+}
