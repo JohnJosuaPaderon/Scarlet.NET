052d193444ab9eb6a92ac21f3f6cadf0c7370f7e
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-31 15:59:11 +0800

    Added NpgsqlCommandCreator.cs Added NpgsqlConnectionEstablisher.cs Added NpgsqlParameterCreator.cs Added NpgsqlProcessHelper.cs Added NpgsqlProcessHelper.Definitions.ExecuteNonQuery.cs Added NpgsqlProcessHelper.Definitions.ExecuteNonQueryAsync.cs Added NpgsqlProcessHelper.Definitions.ExecuteReader.cs Added NpgsqlProcessHelper.Definitions.ExecuteReaderIEnumerable.cs Added NpgsqlProcessHelper.Definitions.ExecuteScalar.cs

---
+++ Scarlet.NetFramework.Npgsql/Rdbms/NpgsqlConnectionEstablisher.cs
@@ -0,0 +1,75 @@
+﻿using Npgsql;
+using Scarlet.Supports;
+using System;
+using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scarlet.Rdbms
+{
+    public sealed class NpgsqlConnectionEstablisher : IDbConnectionEstablisher<NpgsqlConnection>
+    {
+        public NpgsqlConnectionEstablisher(SecureString secureConnectionString)
+        {
+            SecureConnectionString = secureConnectionString ?? throw new ArgumentNullException(nameof(secureConnectionString));
+        }
+
+        public SecureString SecureConnectionString { get; }
+
+        public void Dispose()
+        {
+            SecureConnectionString.Dispose();
+        }
+
+        public NpgsqlConnection Establish()
+        {
+            var connection = new NpgsqlConnection()
+            {
+                ConnectionString = SecureStringConverter.FromSecureString(SecureConnectionString)
+            };
+
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return connection;
+        }
+
+        public async Task<NpgsqlConnection> EstablishAsync()
+        {
+            var connection = new NpgsqlConnection(SecureStringConverter.FromSecureString(SecureConnectionString));
+
+            try
+            {
+                await connection.OpenAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return connection;
+        }
+
+        public async Task<NpgsqlConnection> EstablishAsync(CancellationToken cancellationToken)
+        {
+            var connection = new NpgsqlConnection(SecureStringConverter.FromSecureString(SecureConnectionString));
+
+            try
+            {
+                await connection.OpenAsync(cancellationToken);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return connection;
+        }
+    }
+}
---
+++ Scarlet.NetFramework.Npgsql/Rdbms/NpgsqlProcessHelper.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using System;
+using System.Security;
+
+namespace Scarlet.Rdbms
+{
+    public sealed partial class NpgsqlProcessHelper : IDbProcessHelper<NpgsqlConnection, NpgsqlTransaction, NpgsqlCommand, NpgsqlParameter, NpgsqlDataReader>, IDisposable
+    {
+        public NpgsqlProcessHelper(SecureString secureConnectionString)
+        {
+            ConnectionEstablisher = new NpgsqlConnectionEstablisher(secureConnectionString);
+        }
+        
+        private NpgsqlConnectionEstablisher ConnectionEstablisher { get; set; }
+
+        public void Dispose()
+        {
+            ConnectionEstablisher.Dispose();
+            ConnectionEstablisher = null;
+        }
+    }
+}
