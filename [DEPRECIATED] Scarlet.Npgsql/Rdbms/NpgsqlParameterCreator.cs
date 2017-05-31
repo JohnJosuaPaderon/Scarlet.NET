1e6e2d88c8ab6a3c01a2de58dbc1b2942c8f1d76
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-31 16:01:45 +0800

    Removed Scarlet.Npgsql project.

---
+++ [DEPRECIATED] Scarlet.Npgsql/Rdbms/NpgsqlParameterCreator.cs
@@ -0,0 +1,52 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+
+namespace Scarlet.Rdbms
+{
+    public sealed class NpgsqlParameterCreator : IDbParameterCreator<NpgsqlParameter>
+    {
+        public NpgsqlParameter CreateInOut(string parameterName, object initialValue)
+        {
+            ValidateParameterName(parameterName);
+
+            return new NpgsqlParameter()
+            {
+                ParameterName = parameterName,
+                Direction = ParameterDirection.InputOutput,
+                Value = initialValue
+            };
+        }
+
+        public NpgsqlParameter CreateInput(string parameterName, object value)
+        {
+            ValidateParameterName(parameterName);
+
+            return new NpgsqlParameter()
+            {
+                ParameterName = parameterName,
+                Direction = ParameterDirection.Input,
+                Value = value
+            };
+        }
+
+        public NpgsqlParameter CreateOutput(string parameterName)
+        {
+            ValidateParameterName(parameterName);
+
+            return new NpgsqlParameter()
+            {
+                ParameterName = parameterName,
+                Direction = ParameterDirection.Output
+            };
+        }
+
+        private void ValidateParameterName(string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(parameterName))
+            {
+                throw new ArgumentException("Invalid Parameter Name", nameof(parameterName));
+            }
+        }
+    }
+}
