ce30c8413ac551e62e13592f6ae01b61b2648683
johnpad53 <johnjosua.paderon@outlook.com>
2017-05-31 09:52:17 +0800

    Updated Configuration.cs

--- Scarlet.NetStandard/Core/Configuration.cs
+++ Scarlet.NetStandard/Core/Configuration.cs
@@ -3,9 +3,11 @@
     public static class Configuration
     {
         private static readonly char Default_PersonMiddleNameSeparator = ' ';
+        private static readonly string Default_SystemModuleCodeSeriesFormat = "0000";
 
         private static char _PersonMiddleNameSeparator;
-        
+        private static string _SystemModuleCodeSeriesFormat;
+
         public static char PersonMiddleNameSeparator
         {
             get { return _PersonMiddleNameSeparator; }
@@ -18,6 +20,22 @@
                 else
                 {
                     _PersonMiddleNameSeparator = value;
+                }
+            }
+        }
+
+        public static string SystemModuleCodeSeriesFormat
+        {
+            get { return _SystemModuleCodeSeriesFormat; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    _SystemModuleCodeSeriesFormat = Default_SystemModuleCodeSeriesFormat;
+                }
+                else
+                {
+                    _SystemModuleCodeSeriesFormat = value.Trim();
                 }
             }
         }
